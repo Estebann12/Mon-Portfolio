f781e0dd7f2869f2e4e7c3eba2f6ac89cfb30d9d
Estebann <estebannkrebs2@gmail.com>
2024-01-07 23:56:42 +0100

    ajout php

--- php/contact.php
+++ php/contact.php
@@ -1,3 +1,12 @@
 <?php
+if (isset($_POST["email"])) {
+    $message = "Ce message vous a été envoyé via la page contact du site Mon portfolio
+    Nom : " . $_POST["nom"] . "
+    Email : " . $_POST["email"] . "
+    Message : " . $_POST["message"];
 
-mail("estebannkrebs2@gmail.com", "Essai", "Coucou", "");
+ $retour = mail("estebannkrebs2@gmail.com", $_POST["objet"], $message, "Reply-to:" . $_POST["email"]);
+ if ($retour) {
+    echo "<p>L'email à bien été envoyé</p>"; 
+}
+}
